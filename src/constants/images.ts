9282b7125366cefe77f7a20f2a36eb482a470828
Николай Вовк <n.vovk@zebrains.team>
2019-08-27 20:40:50 +0300

    [GYRE-634] Remove unused icons. Add welcome logo

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,19 +1,14 @@
+// welcome
+export const WELCOME_LOGO = require('../assets/images/welcome/selcome_logo.png')
+
 // tabs
-export const TAB_HOME = require('../assets/images/navigation/tab_home.png')
-export const TAB_HOME_SELECTED = require('../assets/images/navigation/tab_home_selected.png')
-export const TAB_SELECTIONS = require('../assets/images/navigation/tab_selections.png')
-export const TAB_SELECTIONS_SELECTED = require('../assets/images/navigation/tab_selections_selected.png')
 export const TAB_RADIO = require('../assets/images/navigation/tab_radio.png')
-export const TAB_RADIO_SELECTED = require('../assets/images/navigation/tab_radio_selected.png')
 export const TAB_BLOG = require('../assets/images/navigation/tab_blog.png')
-export const TAB_BLOG_SELECTED = require('../assets/images/navigation/tab_blog_selected.png')
 export const TAB_SEARCH = require('../assets/images/navigation/tab_search.png')
-export const TAB_SEARCH_SELECTED = require('../assets/images/navigation/tab_search_selected.png')
 
 export const NAVBAR_SHEVRON_DOWN_WHITE = require('../assets/images/navigation/shevron_down_white.png')
 export const NAVBAR_SETTINGS_WHITE = require('../assets/images/navigation/settings_white.png')
 export const NAVBAR_NOTIFICATIONS_WHITE = require('../assets/images/navigation/notifications_white.png')
-export const NAVBAR_NOTIFICATIONS_BLACK = require('../assets/images/navigation/notifications_black.png')
 export const NAVBAR_AVATAR_EMPTY = require('../assets/images/navigation/avatar_empty.png')
 
 // social
@@ -31,8 +26,6 @@ export const FORM_SEARCH = require('../assets/images/form/search.png')
 export const FORM_SHEVRON_DOWN = require('../assets/images/form/shevron_down.png')
 
 // control
-export const CONTROL_HEART_BLACK = require('../assets/images/control/heart_black.png')
-export const CONTROL_HEART_WHITE = require('../assets/images/control/heart_white.png')
 export const CONTROL_HEART_GRADIENT = require('../assets/images/control/heart_gradient.png')
 export const CONTROL_RIGHT_ARROW_BLACK = require('../assets/images/control/right_arrow_black.png')
 export const CONTROL_HOT = require('../assets/images/control/hot.png')
@@ -44,14 +37,10 @@ export const CONTROL_TRACK_PLAY = require('../assets/images/control/track_play.p
 export const CONTROL_TRACK_PAUSE = require('../assets/images/control/track_pause.png')
 export const CONTROL_BOTTOM_PLAYER_PLAY = require('../assets/images/control/bottom_player_play.png')
 export const CONTROL_BOTTOM_PLAYER_PAUSE = require('../assets/images/control/bottom_player_pause.png')
-export const CONTROL_DOTS_WHITE = require('../assets/images/control/dots_white.png')
 export const CONTROL_DOTS_BLACK = require('../assets/images/control/dots_black.png')
 export const CONTROL_LINES_WHITE = require('../assets/images/control/lines_white.png')
-export const CONTROL_PLAYLIST_BLACK = require('../assets/images/control/playlist_black.png')
 export const CONTROL_PLAYLIST_WHITE = require('../assets/images/control/playlist_white.png')
-export const CONTROL_PLUS_BLACK = require('../assets/images/control/plus_black.png')
 export const CONTROL_PLUS_WHITE = require('../assets/images/control/plus_white.png')
-export const CONTROL_SHUFFLE_BLACK = require('../assets/images/control/shuffle_black.png')
 export const CONTROL_SHUFFLE_WHITE = require('../assets/images/control/shuffle_white.png')
 export const CONTROL_SLIDER_ARROW_LEFT = require('../assets/images/control/slider_arrow_left.png')
 export const CONTROL_SLIDER_ARROW_RIGHT = require('../assets/images/control/slider_arrow_right.png')
@@ -59,27 +48,3 @@ export const CONTROL_SLIDER_ARROW_RIGHT = require('../assets/images/control/slid
 // music
 export const MUSIC_ARTWORK_EMPTY_ALBUM = require('../assets/images/music/artwork_empty_album.png')
 export const MUSIC_ARTWORK_EMPTY_TRACK = require('../assets/images/music/artwork_empty_track.png')
-
-// temp categories
-export const TEMP_CATEGORY_1 = require('../assets/images/temp/categories/category_1.png')
-export const TEMP_CATEGORY_2 = require('../assets/images/temp/categories/category_2.png')
-export const TEMP_CATEGORY_3 = require('../assets/images/temp/categories/category_3.png')
-export const TEMP_CATEGORY_4 = require('../assets/images/temp/categories/category_4.png')
-export const TEMP_CATEGORY_5 = require('../assets/images/temp/categories/category_5.png')
-export const TEMP_CATEGORY_6 = require('../assets/images/temp/categories/category_6.png')
-export const TEMP_CATEGORY_7 = require('../assets/images/temp/categories/category_7.png')
-export const TEMP_CATEGORY_8 = require('../assets/images/temp/categories/category_8.png')
-
-// temp main
-export const TEMP_EVENT = require('../assets/images/temp/main/event.png')
-export const TEMP_DANCE_GIRL = require('../assets/images/temp/main/dance_girl.png')
-export const TEMP_GUITAR_GIRL = require('../assets/images/temp/main/guitar_girl.png')
-export const TEMP_EXPERT_JULIA = require('../assets/images/temp/main/expert_julia.png')
-export const TEMP_EXPERT_MIHAILOV = require('../assets/images/temp/main/expert_mihailov.png')
-export const TEMP_EXPERT_SPLIN = require('../assets/images/temp/main/expert_splin.png')
-export const TEMP_MELOMAN_DJ = require('../assets/images/temp/main/meloman_dj.png')
-export const TEMP_MELOMAN_GUITAR_GIRL = require('../assets/images/temp/main/meloman_guitar_girl.png')
-
-// temp player
-export const TEMP_ARTWORK_MIN = require('../assets/images/temp/player/artwork_min.png')
-export const TEMP_ARTWORK_MAX = require('../assets/images/temp/player/artwork_max.png')
