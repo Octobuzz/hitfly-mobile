c1c9fc9514d688a1b559a7645d136010cea6aa7d
Николай Вовк <n.vovk@zebrains.team>
2019-08-28 17:28:24 +0300

    [GYRE-635] Add Social button component

---
+++ src/components/buttons/SocialButton.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import TextBase from 'src/components/TextBase'
+import { images } from 'src/constants'
+import styled from 'src/styled-components'
+
+const Wrapper = styled.TouchableOpacity``
+
+const Icon = styled.Image``
+
+interface Props {
+  type: 'vk' | 'fb' | 'ok' | 'gg'
+  style?: any
+  onPress?: () => any
+}
+
+class SocialButton extends React.Component<Props> {
+  private getIconSource = (): number => {
+    const { type } = this.props
+    switch (type) {
+      case 'vk':
+        return images.SOCIAL_VKONTAKTE
+      case 'fb':
+        return images.SOCIAL_FACEBOOK
+      case 'ok':
+        return images.SOCIAL_ODNOKLASSNIKI
+      case 'gg':
+        return images.SOCIAL_GOOGLE
+    }
+  }
+
+  render() {
+    const { onPress, style } = this.props
+    const source = this.getIconSource()
+    return (
+      <Wrapper onPress={onPress} style={style}>
+        <Icon source={source} />
+      </Wrapper>
+    )
+  }
+}
+
+export default SocialButton
--- storybook/stories.tsx
+++ storybook/stories.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react-native'
-import { Button, Link } from 'src/components'
+import { Button, Link, SocialButton } from 'src/components'
+import styled from 'src/styled-components'
 
 storiesOf('Button', module)
   .add('Gradient', () => <Button title="Gradient" type="gradient" />)
   .add('Outline', () => <Button title="Outline" type="outline" />)
 storiesOf('Link', module).add('Default', () => <Link title="Link" />)
+
+const CenterContainer = styled.View`
+  align-items: center;
+  justify-content: space-around;
+  flex: 1;
+`
+storiesOf('SocialButton', module).add('Default', () => (
+  <CenterContainer>
+    <SocialButton type="vk" />
+    <SocialButton type="gg" />
+    <SocialButton type="fb" />
+    <SocialButton type="ok" />
+  </CenterContainer>
+))
