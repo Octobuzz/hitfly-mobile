1f88fa209a68f59858386cbd6ff93646da2950fb
Николай Вовк <n.vovk@zebrains.team>
2019-08-28 14:47:59 +0300

    [GYRE-634] Fix fonts using

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,12 +1,20 @@
 import styled from 'src/styled-components'
 
+enum FontFamilies {
+  light = 'GothamPro-Light',
+  regular = 'GothamPro-Regular',
+  bold = 'GothamPro-Bold',
+  black = 'GothamPro-Black',
+}
+
 interface ITextBase {
   color?: string
+  fontType?: 'light' | 'regular' | 'bold' | 'black'
 }
 
 const TextBase = styled.Text<ITextBase>`
-  font-family: 'GothamPro';
-  font-weight: 400;
+  font-family: ${({ fontType }) =>
+    fontType ? FontFamilies[fontType] : FontFamilies.regular};
   font-size: 16px;
   color: ${({ color, theme }) => color || theme.colors.textMain};
 `
