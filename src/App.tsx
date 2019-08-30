4a806429edbb2b7a01e0b551d9ac269e89de4269
Николай Вовк <n.vovk@zebrains.team>
2019-08-30 15:34:30 +0300

    [GYRE-635] Install apollo. Fix fonts on ios

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { YellowBox } from 'react-native'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
 import AppNavigator from './navigation'
 import { configureStore } from './redux'
 import theme from './theme'
@@ -13,14 +15,20 @@ YellowBox.ignoreWarnings([
   'Warning: componentWillReceiveProps is deprecated',
 ])
 
-const store = configureStore()
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+})
+
+// const store = configureStore()
 
 const App = () => (
-  <Provider store={store}>
+  <ApolloProvider client={client}>
+    {/* <Provider store={store}> */}
     <ThemeProvider theme={theme}>
       <AppNavigator />
     </ThemeProvider>
-  </Provider>
+    {/* </Provider> */}
+  </ApolloProvider>
 )
 
 export default App
