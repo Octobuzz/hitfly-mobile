91c549850c9214ffd5512ff7046412eb153625ed
Николай Вовк <n.vovk@zebrains.team>
2019-09-01 15:42:58 +0300

    [GYRE-639] Add SectionHeader component

--- src/screens/Home/GenresSection.tsx
+++ src/screens/Home/GenresSection.tsx
@@ -1,9 +1,25 @@
+import R from 'ramda'
 import React from 'react'
+import {
+  View,
+  Loader,
+  Button,
+  GenreItem,
+  HelperText,
+  IGenreItem,
+} from 'src/components'
 import styled from 'src/styled-components'
+import SectionHeader from './SectionHeader'
+
+const Wrapper = styled.View``
 
 class GenresSection extends React.Component {
   render() {
-    return null
+    return (
+      <Wrapper>
+        <SectionHeader title="Жанры" />
+      </Wrapper>
+    )
   }
 }
 
---
+++ src/screens/Home/SectionHeader.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { TextBase } from 'src/components'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import styled from 'src/styled-components'
+
+const Wrapper = styled.TouchableOpacity``
+
+const Inner = styled.View`
+  align-items: center;
+  flex-direction: row;
+`
+
+const TitleText = styled(TextBase)`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: 20px;
+  margin-right: 8px;
+`
+
+const SubtitleText = styled(TextBase)`
+  color: ${({ theme }) => theme.colors.textAlt};
+  font-size: 12px;
+  margin-top: 7px;
+`
+
+interface Props {
+  onPress?: () => void
+  title: string
+  subtitle?: string
+}
+
+const SectionHeader: React.FC<Props> = ({ title, subtitle, onPress }) => (
+  <Wrapper onPress={onPress}>
+    <Inner>
+      <TitleText>{title}</TitleText>
+      <Icon name="arrow-forward" size={20} />
+    </Inner>
+    {subtitle && <SubtitleText>{subtitle}</SubtitleText>}
+  </Wrapper>
+)
+
+export default SectionHeader
