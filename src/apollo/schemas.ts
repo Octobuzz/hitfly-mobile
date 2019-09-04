02f72468d4319160cb920482256ab8717045274b
Николай Вовк <n.vovk@zebrains.team>
2019-09-04 17:26:02 +0300

    [GYRE-639] Add type Track

--- src/apollo/schemas.ts
+++ src/apollo/schemas.ts
@@ -1,8 +1,10 @@
+// Типы обявлены с их алиасами (в комментах), поэтому не забывай их добавлять в запросы.
+// алиасы добавлены, потому что мне не нравятся названия переменных с бэка
 export interface Genre {
   id: number
-  title: string
-  imageUrl: string
-  userFavourite: boolean
+  title: string // name
+  imageUrl: string // image
+  isFavourite: boolean // userFavourite
   countTracks: number
   countListenedByUser?: number
 }
@@ -14,7 +16,29 @@ export type SocialType =
   | 'odnoklassniki'
 
 export interface SocialConnect {
-  url: string
-  type: SocialType
-  isLinked: boolean
+  url: string // link
+  type: SocialType // social_type
+  isLinked: boolean // connected
+}
+
+export interface Track {
+  id: number
+  title: string // trackName
+  genres: Genre[]
+  album: any // TODO: добавить Album
+  uploadedBy: any // user TODO: добавить User
+  group: any // musucGroup TODO: добавить MusicGroup
+  singer: string
+  date: string // trackDate
+  songText: string
+  fileUrl: string // filename
+  isFavourite: boolean // userFavourite
+  favouritesCount: number
+  userPlayLists: any // не знаю что это  TODO: добавить Collection[]
+  musicWave: number[]
+  cover: any // TODO: добавить ImageSizesType[]
+  comments: any // TODO: добавить CommentTrack[]
+  length?: number
+  isMine: boolean // my
+  commentedByMe: boolean
 }
