789d0110c13befb662e7efdfe694cb8a2ae7042f
Николай Вовк <n.vovk@zebrains.team>
2019-09-19 13:04:06 +0300

    [GYRE-731] Add Playlist header

--- src/screens/Playlist/Playlist.tsx
+++ src/screens/Playlist/Playlist.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import FastImage, { FastImageSource } from 'react-native-fast-image'
+import ShuffleButton from './ShuffleButton'
+import { Track } from 'src/apollo'
 import styled from 'src/styled-components'
 
-class Playlist extends React.Component {
+const CoverWrapper = styled.View`
+  height: 45%;
+  justify-content: flex-end;
+  background-color: ${({ theme }) => theme.colors.white};
+`
+
+const Cover = styled(FastImage)`
+  border-bottom-left-radius: 28px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
+
+const PositionedShuffleButton = styled(ShuffleButton)`
+  align-self: flex-end;
+  width: 80%;
+`
+
+interface Props {
+  cover: FastImageSource | number
+  tracks: Track[]
+  favouriteCount: number
+}
+
+class Playlist extends React.Component<Props> {
   render() {
-    return null
+    const { cover } = this.props
+    return (
+      <CoverWrapper>
+        <Cover source={cover} />
+        <PositionedShuffleButton />
+      </CoverWrapper>
+    )
   }
 }
 
--- storybook/stories.tsx
+++ storybook/stories.tsx
@@ -11,6 +11,7 @@ import {
   SocialButton,
   TextWithLines,
 } from 'src/components'
+import { PlaylistScreen } from 'src/screens'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import styled from 'src/styled-components'
 
@@ -114,3 +115,14 @@ const GenreItemStory = () => {
 }
 
 storiesOf('GenreItem', module).add('Default', () => <GenreItemStory />)
+
+storiesOf('PlaylistScreen', module).add('Default', () => (
+  <PlaylistScreen
+    cover={{
+      uri:
+        'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_960_720.jpg',
+    }}
+    tracks={[]}
+    favouriteCount={100}
+  />
+))
