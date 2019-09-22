7c5f2ef55ca9c38505d9eccd8c2aa329aae23d5a
Николай Вовк <n.vovk@zebrains.team>
2019-09-22 03:34:43 +0300

    [GYRE-732] Remove dead images. Add new images

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,6 +3,8 @@ export const TOP50_BACKGROUND = require('../assets/images/home_sections/top50_ba
 export const TOP50_PLAYLIST = require('../assets/images/home_sections/top50_playlist.png')
 export const LISTENED_NOW = require('../assets/images/home_sections/listened_now.png')
 export const LISTENED_NOW_PLAYLIST = require('../assets/images/home_sections/listened_now_playlist.png')
+export const TOP_WEEK_PLAYLIST = require('../assets/images/home_sections/top_week_playlist.png')
+export const NEW_PLAYLIST = require('../assets/images/home_sections/new_playlist.png')
 
 // genres
 export const GENRE_INACTIVE = require('../assets/images/genres/genre_inactive.png')
@@ -19,42 +21,12 @@ export const GROWTH_GRADIENT = require('../assets/images/welcome/growth_gradient
 export const HEADPHONES_GRADIENT = require('../assets/images/welcome/headphones_gradient.png')
 export const INSIGNIA_GRADIENT = require('../assets/images/welcome/insignia_gradient.png')
 
-// tabs
-export const TAB_RADIO = require('../assets/images/navigation/tab_radio.png')
-export const TAB_BLOG = require('../assets/images/navigation/tab_blog.png')
-export const TAB_SEARCH = require('../assets/images/navigation/tab_search.png')
-
-export const NAVBAR_SHEVRON_DOWN_WHITE = require('../assets/images/navigation/shevron_down_white.png')
-export const NAVBAR_SETTINGS_WHITE = require('../assets/images/navigation/settings_white.png')
-export const NAVBAR_NOTIFICATIONS_WHITE = require('../assets/images/navigation/notifications_white.png')
-export const NAVBAR_AVATAR_EMPTY = require('../assets/images/navigation/avatar_empty.png')
-
 // social
 export const SOCIAL_FACEBOOK = require('../assets/images/social/facebook.png')
 export const SOCIAL_VKONTAKTE = require('../assets/images/social/vkontakte.png')
 export const SOCIAL_GOOGLE = require('../assets/images/social/google.png')
 export const SOCIAL_INSTAGRAM = require('../assets/images/social/instagram.png')
 export const SOCIAL_ODNOKLASSNIKI = require('../assets/images/social/odnoklassniki.png')
-
-// control
-export const CONTROL_HEART_GRADIENT = require('../assets/images/control/heart_gradient.png')
-export const CONTROL_RIGHT_ARROW_BLACK = require('../assets/images/control/right_arrow_black.png')
-export const CONTROL_HOT = require('../assets/images/control/hot.png')
-export const CONTROL_PLAYER_PLAY = require('../assets/images/control/player_play.png')
-export const CONTROL_PLAYER_PAUSE = require('../assets/images/control/player_pause.png')
-export const CONTROL_PLAYER_PREV = require('../assets/images/control/player_prev.png')
-export const CONTROL_PLAYER_NEXT = require('../assets/images/control/player_next.png')
-export const CONTROL_TRACK_PLAY = require('../assets/images/control/track_play.png')
-export const CONTROL_TRACK_PAUSE = require('../assets/images/control/track_pause.png')
-export const CONTROL_BOTTOM_PLAYER_PLAY = require('../assets/images/control/bottom_player_play.png')
-export const CONTROL_BOTTOM_PLAYER_PAUSE = require('../assets/images/control/bottom_player_pause.png')
-export const CONTROL_DOTS_BLACK = require('../assets/images/control/dots_black.png')
-export const CONTROL_LINES_WHITE = require('../assets/images/control/lines_white.png')
-export const CONTROL_PLAYLIST_WHITE = require('../assets/images/control/playlist_white.png')
-export const CONTROL_PLUS_WHITE = require('../assets/images/control/plus_white.png')
-export const CONTROL_SHUFFLE_WHITE = require('../assets/images/control/shuffle_white.png')
-export const CONTROL_SLIDER_ARROW_LEFT = require('../assets/images/control/slider_arrow_left.png')
-export const CONTROL_SLIDER_ARROW_RIGHT = require('../assets/images/control/slider_arrow_right.png')
 
 // music
 export const MUSIC_ARTWORK_EMPTY_ALBUM = require('../assets/images/music/artwork_empty_album.png')
