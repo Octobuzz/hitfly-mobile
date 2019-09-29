0c87b0c2529e0a806fc0e24f4ad0ea70b5ee3932
Николай Вовк <n.vovk@zebrains.team>
2019-09-29 16:01:34 +0300

    [GYRE-643] Add tests for GenreItem

--- __tests__/components/GenreItem.test.tsx
+++ __tests__/components/GenreItem.test.tsx
@@ -2,17 +2,39 @@ import React from 'react'
 import { GenreItem } from 'src/components'
 import { Genre } from 'src/apollo'
 import { ThemeProvider } from 'src/styled-components'
-import renderer from 'react-test-renderer'
+import { render } from '../../jest/test-utils'
 import theme from 'src/theme'
 
-test('renders correctly', () => {
+describe('GenreItem', () => {
   const item = { title: 'title', imageUrl: 'url' } as Genre
-  const tree = renderer
-    .create(
+
+  it('renders correctly', () => {
+    const { asJSON } = render(
       <ThemeProvider theme={theme}>
         <GenreItem item={item} />
       </ThemeProvider>,
     )
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+
+    expect(asJSON()).toMatchSnapshot()
+  })
+
+  it('renders correctly in selected state', () => {
+    const { asJSON } = render(
+      <ThemeProvider theme={theme}>
+        <GenreItem isSelectable isSelected item={item} />
+      </ThemeProvider>,
+    )
+
+    expect(asJSON()).toMatchSnapshot()
+  })
+
+  it('renders correctly in unselected state', () => {
+    const { asJSON } = render(
+      <ThemeProvider theme={theme}>
+        <GenreItem isSelectable isSelected={false} item={item} />
+      </ThemeProvider>,
+    )
+
+    expect(asJSON()).toMatchSnapshot()
+  })
 })
