84d3cd6c7d553ad550283a2d75a85a55beb9137f
Николай Вовк <n.vovk@zebrains.team>
2019-10-08 12:56:52 +0300

    Fix android issues

--- android/app/src/main/java/com/zebrains/hitfly/MainApplication.java
+++ android/app/src/main/java/com/zebrains/hitfly/MainApplication.java
@@ -13,8 +13,7 @@ import java.lang.reflect.InvocationTargetException;
 
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication{
-
+public class MainApplication extends Application implements ReactApplication {
 
 
     private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -51,28 +50,30 @@ public class MainApplication extends Application implements ReactApplication{
     }
 
 
-  /**
-   * Loads Flipper in React Native templates.
-   *
-   * @param context
-   */
-  private static void initializeFlipper(Context context) {
-    if (BuildConfig.DEBUG) {
-      try {
+    /**
+     * Loads Flipper in React Native templates.
+     *
+     * @param context
+     */
+    private static void initializeFlipper(Context context) {
+        if (BuildConfig.DEBUG) {
+            try {
         /*
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
-        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
+                Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+                aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
