01a2af37ceb73d9c2bc2a869998f783020b522b0
Николай Вовк <n.vovk@zebrains.team>
2019-10-08 18:35:33 +0300

    Add firease for iOs. Fix fastfile

--- ios/hitfly/AppDelegate.m
+++ ios/hitfly/AppDelegate.m
@@ -12,11 +12,13 @@
 #import <React/RCTRootView.h>
 #import "RNSplashScreen.h"
 
+@import Firebase;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  
+  [FIRApp configure];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"hitfly"
