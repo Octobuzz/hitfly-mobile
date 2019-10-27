e1851e7b2fdc9058e7a496e884ca3339f26f763d
Николай Вовк <n.vovk@zebrains.team>
2019-10-27 12:50:00 +0300

    [refactoring] Refactor TextInput

--- src/components/fields/TextInputUI.tsx
+++ src/components/fields/TextInputUI.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { TextInputProps } from 'react-native'
 // @ts-ignore
 import { OutlinedTextField } from 'react-native-material-textfield'
@@ -12,21 +12,34 @@ const TextInput = styled(OutlinedTextField).attrs(({ theme }) => ({
   labelTextStyle: {
     fontFamily: theme.fonts.regular,
   },
-  // labelHeight: 20,
 }))`
   color: ${({ theme }) => theme.colors.textMain};
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: 12px;
 `
 
-class TextInputUI extends React.Component<InputBase & TextInputProps> {
-  private renderAccessory = (): JSX.Element => {
-    const { RightIcon } = this.props
-    return RightIcon
-  }
+const TextInputUI: React.FC<InputBase & TextInputProps> = ({
+  value,
+  RightIcon,
+  ...rest
+}) => {
+  const renderRightAccessory = (): JSX.Element => RightIcon
 
-  render() {
-    return <TextInput renderAccessory={this.renderAccessory} {...this.props} />
-  }
+  const ref = useRef(null)
+
+  useEffect(() => {
+    if (ref && ref.current) {
+      // @ts-ignore
+      ref.current.setValue(value)
+    }
+  }, [value])
+
+  return (
+    <TextInput
+      ref={ref}
+      renderRightAccessory={renderRightAccessory}
+      {...rest}
+    />
+  )
 }
 export default TextInputUI
