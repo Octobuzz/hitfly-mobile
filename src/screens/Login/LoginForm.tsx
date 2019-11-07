7f9cebb518952a923f135bdff3fbb680420f1a4c
Николай Вовк <n.vovk@zebrains.team>
2019-11-07 09:45:27 +0300

    [GYRE-647] Add ref for inputs

--- src/screens/Login/LoginForm.tsx
+++ src/screens/Login/LoginForm.tsx
@@ -1,8 +1,7 @@
-import L from 'lodash'
-import React from 'react'
+import React, { useRef, useCallback } from 'react'
 import * as Yup from 'yup'
 import { Field, FormikProps, withFormik } from 'formik'
-import { strings, storageKeys } from 'src/constants'
+import { strings } from 'src/constants'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import SimpleLineIcon from 'react-native-vector-icons/SimpleLineIcons'
 import { Link, Input, Button } from 'src/components'
@@ -34,36 +33,50 @@ const LoginForm: React.FC<Props> = ({
   handleSubmit,
   isSubmitting,
   onPressFogrotPassword,
-}) => (
-  <>
-    <Field
-      name="email"
-      label="E-mail"
-      component={IndentedInput}
-      textContentType="emailAddress"
-      keyboardType="email-address"
-      RightIcon={<MaterialIcon size={20} name="mail-outline" />}
-    />
-    <Field
-      name="password"
-      label="Пароль"
-      textContentType="password"
-      enablesReturnKeyAutomatically
-      secureTextEntry
-      component={IndentedInput}
-      returnKeyType="send"
-      onSubmitEditing={handleSubmit}
-      RightIcon={<SimpleLineIcon size={20} name="key" />}
-    />
-    <IndentedLink onPress={onPressFogrotPassword} title="Забыли пароль?" />
-    <Button
-      title="Войти"
-      isDisabled={!isValid || isSubmitting}
-      isLoading={isSubmitting}
-      onPress={handleSubmit}
-    />
-  </>
-)
+}) => {
+  const passwordRef = useRef()
+  const focusPasswordField = useCallback(() => {
+    if (passwordRef && passwordRef.current) {
+      // @ts-ignore
+      passwordRef.current.focus()
+    }
+  }, [passwordRef])
+
+  return (
+    <>
+      <Field
+        name="email"
+        label="E-mail"
+        component={IndentedInput}
+        onSubmitEditing={focusPasswordField}
+        enablesReturnKeyAutomatically
+        returnKeyType="next"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+        RightIcon={<MaterialIcon size={20} name="mail-outline" />}
+      />
+      <Field
+        name="password"
+        label="Пароль"
+        forwardRef={passwordRef}
+        textContentType="password"
+        enablesReturnKeyAutomatically
+        secureTextEntry
+        component={IndentedInput}
+        returnKeyType="send"
+        onSubmitEditing={handleSubmit}
+        RightIcon={<SimpleLineIcon size={20} name="key" />}
+      />
+      <IndentedLink onPress={onPressFogrotPassword} title="Забыли пароль?" />
+      <Button
+        title="Войти"
+        isDisabled={!isValid || isSubmitting}
+        isLoading={isSubmitting}
+        onPress={handleSubmit}
+      />
+    </>
+  )
+}
 
 const validationSchema = Yup.object().shape({
   email: Yup.string()
