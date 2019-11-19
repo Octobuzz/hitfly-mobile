e54da72fb19c095196b41f390e0b4533ee150061
Николай Вовк <n.vovk@zebrains.team>
2019-11-19 13:39:05 +0300

    [GYRE-640] Add comments

--- src/components/Slider/index.ts
+++ src/components/Slider/index.ts
@@ -1,2 +1,6 @@
+// Slider одолжен из react-native-slider
+// однако он заброшен и много вещей устарело
+// так же добавил некоторые PR сюда
+// наверно стоит залить в отдельный NPM модуль
 export { default } from './Slider'
 export { default as StyledSlider } from './StyledSlider'
