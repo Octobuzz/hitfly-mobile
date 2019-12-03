c4d754dde40824a12927b8198c574d5099f184d8
Николай Вовк <n.vovk@zebrains.team>
2019-12-03 14:43:12 +0300

    [GYRE-1158] Fix wrong progress on slider

--- src/containers/components/PlayerSlider.tsx
+++ src/containers/components/PlayerSlider.tsx
@@ -1,7 +1,10 @@
+import L from 'lodash'
 import React, { useCallback, useMemo } from 'react'
+import { useQuery } from '@apollo/react-hooks'
 import TrackPlayer from 'react-native-track-player'
 // @ts-ignore больные ублюдки
 import { useTrackPlayerProgress } from 'react-native-track-player/lib/hooks'
+import { ActiveTrackData, GET_ACTIVE_TRACK } from 'src/apollo'
 import { StyledSlider, TextBase } from 'src/components'
 import { addSeconds, format } from 'date-fns'
 import styled from 'src/styled-components'
@@ -36,8 +39,24 @@ const PlayerSlider: React.FC<Props> = () => {
     TrackPlayer.seekTo(progress)
   }, [])
 
-  const textProgress = useMemo(() => formatDuration(position), [position])
-  const textDuration = useMemo(() => formatDuration(duration), [duration])
+  const data = useQuery<ActiveTrackData>(GET_ACTIVE_TRACK)
+
+  const activeTrack = L.get(data, 'data.activeTrack')
+
+  const recentDuration = (activeTrack && activeTrack.length) || duration
+
+  // тут есть баг, из-за которого иногда прогресс больше всей длинны
+  const textProgress = useMemo(
+    () =>
+      position < recentDuration
+        ? formatDuration(position)
+        : formatDuration(recentDuration),
+    [position, recentDuration],
+  )
+  const textDuration = useMemo(() => formatDuration(recentDuration), [
+    duration,
+    recentDuration,
+  ])
 
   return (
     <Wrapper>
