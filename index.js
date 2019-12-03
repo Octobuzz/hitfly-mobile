0d71760cee9c1bebc3f8c25b93bbf1c07df9ea78
Николай Вовк <n.vovk@zebrains.team>
2019-12-03 13:27:08 +0300

    Refactor android icons. Destroy player on exit

--- index.js
+++ index.js
@@ -5,21 +5,6 @@ import TrackPlayer from 'react-native-track-player'
 import { names } from 'src/constants'
 import App from 'src/App'
 
-const setup = async () => {
-  await TrackPlayer.setupPlayer()
-  TrackPlayer.updateOptions({
-    stopWithApp: true,
-    capabilities: [
-      TrackPlayer.CAPABILITY_PLAY,
-      TrackPlayer.CAPABILITY_PAUSE,
-      TrackPlayer.CAPABILITY_SEEK_TO,
-      TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
-      TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
-    ],
-  })
-}
-
 AppRegistry.registerComponent(names.APP_NAME, () => App)
 
-setup()
 TrackPlayer.registerPlaybackService(() => require('./trackService.js'))
--- src/containers/components/Player.tsx
+++ src/containers/components/Player.tsx
@@ -9,6 +9,21 @@ import {
   RESET_PLAYER,
 } from 'src/apollo'
 
+const setup = async (onSuccess: () => any) => {
+  await TrackPlayer.setupPlayer()
+  await TrackPlayer.updateOptions({
+    stopWithApp: true,
+    capabilities: [
+      TrackPlayer.CAPABILITY_PLAY,
+      TrackPlayer.CAPABILITY_PAUSE,
+      TrackPlayer.CAPABILITY_SEEK_TO,
+      TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+      TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+    ],
+  })
+  onSuccess()
+}
+
 const Player: React.FC = () => {
   const [setActiveTrackId] = useMutation<any, SetActiveTrackIdVariables>(
     SET_ACTIVE_TRACK_ID,
@@ -17,41 +32,44 @@ const Player: React.FC = () => {
   const [resetPlayer] = useMutation(RESET_PLAYER)
 
   useEffect(() => {
-    const ended = TrackPlayer.addEventListener(
-      'playback-queue-ended',
-      async () => {
+    let ended: TrackPlayer.EmitterSubscription
+    let changed: TrackPlayer.EmitterSubscription
+    let stateChanged: TrackPlayer.EmitterSubscription
+    setup(() => {
+      ended = TrackPlayer.addEventListener('playback-queue-ended', async () => {
         setIsPlaying({ variables: { isPlaying: false } })
         await TrackPlayer.pause()
         TrackPlayer.seekTo(0)
-      },
-    )
-    const changed = TrackPlayer.addEventListener(
-      'playback-track-changed',
-      async () => {
-        const currentTrackId = await TrackPlayer.getCurrentTrack()
-        setActiveTrackId({ variables: { id: +currentTrackId } })
-      },
-    )
+      })
+      changed = TrackPlayer.addEventListener(
+        'playback-track-changed',
+        async () => {
+          const currentTrackId = await TrackPlayer.getCurrentTrack()
+          setActiveTrackId({ variables: { id: +currentTrackId } })
+        },
+      )
 
-    const stateChanged = TrackPlayer.addEventListener(
-      'playback-state',
-      ({ state }) => {
-        switch (state) {
-          case TrackPlayer.STATE_PAUSED: {
-            setIsPlaying({ variables: { isPlaying: false } })
-            break
+      stateChanged = TrackPlayer.addEventListener(
+        'playback-state',
+        ({ state }) => {
+          switch (state) {
+            case TrackPlayer.STATE_PAUSED: {
+              setIsPlaying({ variables: { isPlaying: false } })
+              break
+            }
+            case TrackPlayer.STATE_PLAYING: {
+              setIsPlaying({ variables: { isPlaying: true } })
+              break
+            }
           }
-          case TrackPlayer.STATE_PLAYING: {
-            setIsPlaying({ variables: { isPlaying: true } })
-            break
-          }
-        }
-      },
-    )
+        },
+      )
+    })
 
     return () => {
       resetPlayer()
       TrackPlayer.reset()
+      TrackPlayer.destroy()
       stateChanged.remove()
       changed.remove()
       ended.remove()
