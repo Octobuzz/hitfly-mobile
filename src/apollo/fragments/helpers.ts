50c8cfdb4e746e2edaf204ae448e258b76046780
Николай Вовк <n.vovk@zebrains.team>
2019-12-04 11:29:23 +0300

    [detailed_track_drawer] Fix adjust function

--- src/apollo/fragments/helpers.ts
+++ src/apollo/fragments/helpers.ts
@@ -2,7 +2,12 @@ import { PixelRatio } from 'react-native'
 
 const MAX_RATIO = 3.5
 
+const ceil = (value: number, step: number = 1.0): number => {
+  const inv = 1.0 / step
+  return Math.ceil(value * inv) / inv
+}
+
 export const getAdjustedPixelRatio = (): number => {
-  const ratio = PixelRatio.get()
+  const ratio = ceil(PixelRatio.get(), 0.5)
   return Math.min(ratio, MAX_RATIO)
 }
