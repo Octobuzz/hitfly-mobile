df030a919770369d3f81f971296738444782a257
Николай Вовк <n.vovk@zebrains.team>
2019-12-13 13:59:30 +0300

    [GYRE-1185] Remove dead code

--- src/containers/components/Player.tsx
+++ src/containers/components/Player.tsx
@@ -7,6 +7,12 @@ import {
   RESET_PLAYER,
 } from 'src/apollo'
 
+interface SkipOptions {
+  canPlayNext: boolean
+  canPlayPrev: boolean
+  currentTrack: string
+}
+
 const Player: React.FC = () => {
   const [setProperties] = useMutation<any, SetPlayerPropertiesVariables>(
     SET_PLAYER_PROPERTIES,
@@ -18,30 +24,8 @@ const Player: React.FC = () => {
     result.push(
       TrackPlayer.addEventListener('playback-queue-ended', () => {
         setProperties({ variables: { isPlaying: false } })
-        // await TrackPlayer.pause()
-        // TrackPlayer.seekTo(0)
       }),
     )
-
-    // result.push(
-    //   TrackPlayer.addEventListener(
-    //     'playback-track-changed',
-    //     async ({ nextTrack, track }) => {
-    //       const queue = await TrackPlayer.getQueue()
-    //       const skipOptions = getSkipOptions<string, TrackPlayer.Track>(
-    //         nextTrack,
-    //         queue,
-    //       )
-    //       console.log({ nextTrack, queue, track })
-    //       setProperties({
-    //         variables: {
-    //           activeTrackId: +nextTrack,
-    //           ...skipOptions,
-    //         },
-    //       })
-    //     },
-    //   ),
-    // )
 
     result.push(
       TrackPlayer.addEventListener('remote-play', () => {
@@ -55,65 +39,29 @@ const Player: React.FC = () => {
       }),
     )
 
-    result.push(
-      TrackPlayer.addEventListener(
-        'remote-next',
-        ({ canPlayNext, canPlayPrev, currentTrack }) => {
-          setProperties({
-            variables: {
-              canPlayNext,
-              canPlayPrev,
-              activeTrackId: +currentTrack,
-            },
-          })
+    const updateSkipProps = ({
+      canPlayNext,
+      canPlayPrev,
+      currentTrack,
+    }: SkipOptions) => {
+      setProperties({
+        variables: {
+          canPlayNext,
+          canPlayPrev,
+          activeTrackId: +currentTrack,
         },
-      ),
+      })
+    }
+
+    result.push(TrackPlayer.addEventListener('remote-next', updateSkipProps))
+
+    result.push(
+      TrackPlayer.addEventListener('remote-previous', updateSkipProps),
     )
 
     result.push(
-      TrackPlayer.addEventListener(
-        'remote-previous',
-        ({ canPlayPrev, canPlayNext, currentTrack }) => {
-          setProperties({
-            variables: {
-              canPlayPrev,
-              canPlayNext,
-              activeTrackId: +currentTrack,
-            },
-          })
-        },
-      ),
+      TrackPlayer.addEventListener('remote-progress-ended', updateSkipProps),
     )
-
-    result.push(
-      TrackPlayer.addEventListener(
-        'remote-progress-ended',
-        ({ canPlayPrev, canPlayNext, currentTrack }) => {
-          setProperties({
-            variables: {
-              canPlayPrev,
-              canPlayNext,
-              activeTrackId: +currentTrack,
-            },
-          })
-        },
-      ),
-    )
-
-    // result.push(
-    //   TrackPlayer.addEventListener('playback-state', ({ state }) => {
-    //     switch (state) {
-    //       case TrackPlayer.STATE_PAUSED: {
-    //         setProperties({ variables: { isPlaying: false } })
-    //         break
-    //       }
-    //       case TrackPlayer.STATE_PLAYING: {
-    //         setProperties({ variables: { isPlaying: true } })
-    //         break
-    //       }
-    //     }
-    //   }),
-    // )
 
     return result
   }, [])
