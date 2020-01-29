56ce917e398d3c72307090559cd1f94002df6cad
Николай Вовк <n.vovk@zebrains.team>
2020-01-29 13:41:18 +0300

    [unit-tests] Add tests for TracksFlatList

--- __tests__/components/TracksFlatList.test.tsx
+++ __tests__/components/TracksFlatList.test.tsx
@@ -12,18 +12,59 @@ describe('TrackFlatList', () => {
     cover: [{}],
   } as Track
 
+  const staticProps: TracksFlatListProps = {
+    toggleTrack: jest.fn(),
+    showDetailedTrack: jest.fn(),
+    tracks: [item],
+    playlistTitle: 'title',
+    onRefresh: jest.fn(),
+    onEndReached: jest.fn(),
+    isRefreshing: false,
+    isFetchingMore: false,
+    playlistKey: 'key',
+    isPlaying: false,
+  }
+
   it('renders correctly in default state', () => {
+    const { asJSON } = render(<TracksFlatList {...staticProps} />)
+    expect(asJSON()).toMatchSnapshot()
+  })
+
+  it('renders correctly in playing state', () => {
     const props: TracksFlatListProps = {
-      toggleTrack: jest.fn(),
-      showDetailedTrack: jest.fn(),
-      tracks: [item],
-      playlistTitle: 'title',
-      onRefresh: jest.fn(),
-      onEndReached: jest.fn(),
-      isRefreshing: false,
-      isFetchingMore: false,
-      playlistKey: 'key',
-      isPlaying: false,
+      ...staticProps,
+      isPlaying: true,
+      activeTrack: item,
+    }
+
+    const { asJSON } = render(<TracksFlatList {...props} />)
+    expect(asJSON()).toMatchSnapshot()
+  })
+
+  it('renders correctly in fetching more state', () => {
+    const props: TracksFlatListProps = {
+      ...staticProps,
+      isFetchingMore: true,
+    }
+
+    const { asJSON } = render(<TracksFlatList {...props} />)
+    expect(asJSON()).toMatchSnapshot()
+  })
+
+  it('renders correctly in refreshing state', () => {
+    const props: TracksFlatListProps = {
+      ...staticProps,
+      isRefreshing: true,
+    }
+
+    const { asJSON } = render(<TracksFlatList {...props} />)
+    expect(asJSON()).toMatchSnapshot()
+  })
+
+  it('renders correctly without items', () => {
+    const props: TracksFlatListProps = {
+      ...staticProps,
+      tracks: [],
     }
 
     const { asJSON } = render(<TracksFlatList {...props} />)
